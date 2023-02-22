ab8b8eb3013d3f41f87f0fdc9331154203021a45
Schwa Aresty <jaresty@gmail.com>
2023-02-21 16:13:43 -0800

    Add lsp support

--- doom.d/config.el
+++ doom.d/config.el
@@ -42,7 +42,6 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
-
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
