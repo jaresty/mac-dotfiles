7afeb546727970b78227e2f1434185111525da2e
Schwa Aresty <jaresty@gmail.com>
2023-03-03 11:01:56 -0800

    Add make and d2-mode packages
    - still need to start d2-mode when opening d2 files

--- doom.d/init.el
+++ doom.d/init.el
@@ -97,7 +97,7 @@
        (lookup +docsets +dictionary +offline)              ; navigate your code and its documentation
        (lsp +peek)               ; M-x vscode
        magit             ; a git porcelain for Emacs
-       ;;make              ; run make tasks from Emacs
+       make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
