59298384c775d2adef9561fd46174d3ad9088fd9
Schwa Aresty <jaresty@gmail.com>
2023-03-05 20:13:18 -0800

    Add GTD org mode and splash image

--- doom.d/config.el
+++ doom.d/config.el
@@ -44,8 +44,22 @@
 
 (after! org
   (setq org-directory "~/org/")
+  (setq org-agenda-files '("~/gtd/inbox.org"
+                           "~/gtd/gtd.org"
+                           "~/gtd/tickler.org"))
+  (setq org-capture-templates '(("t" "Todo [inbox]" entry
+                                 (file+headline "~/gtd/inbox.org" "Tasks")
+                                 "* TODO %i%?")
+                                ("T" "Tickler" entry
+                                 (file+headline "~/gtd/tickler.org" "Tickler")
+                                 "* %i%? \n %U")))
+  (setq org-refile-targets '(("~/gtd/gtd.org" :maxlevel . 3)
+                           ("~/gtd/someday.org" :level . 1)
+                           ("~/gtd/tickler.org" :maxlevel . 2)))
+  (setq org-todo-keywords '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
   )
 
+(setq fancy-splash-image (concat doom-private-dir "splash.png"))
 (after! evil-vars
   (setq evil-shift-width 2)
   )
