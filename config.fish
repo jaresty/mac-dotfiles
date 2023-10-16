de6dcc9ea2456409ce8eeb374383f24555ab82c3
Joshua Aresty <josh.aresty@movehq.com>
2023-10-15 18:27:04 -0700

    auto wrap ap to width

--- config.fish
+++ config.fish
@@ -52,7 +52,7 @@ if status is-interactive
   "
 
     function ap
-        curl -s https://raw.githubusercontent.com/f/awesome-chatgpt-prompts/main/prompts.csv | tail +2 | ruby -rcsv -e 'CSV.parse(STDIN.read) {|row| puts row.join("\t")}' | fzf --with-nth 1 --delimiter "\t" --preview 'echo {2} | fold -w 80'
+        curl -s https://raw.githubusercontent.com/f/awesome-chatgpt-prompts/main/prompts.csv | tail +2 | ruby -rcsv -e 'CSV.parse(STDIN.read) {|row| puts row.join("\t")}' | fzf --with-nth 1 --delimiter "\t" --preview 'echo {2} | fold -s -w $(tput cols)'
     end
 
     set FPATH "$(brew --prefix)/share/zsh/site-functions:{$FPATH}"
