dfc0acee4ad8bb63090b9d42453f7bfeea500b3b
Joshua Aresty <josh.aresty@movehq.com>
2023-10-16 19:33:18 -0700

    configure fzf bindings

--- config.fish
+++ config.fish
@@ -67,7 +67,7 @@ if status is-interactive
     if test -d (brew --prefix)"/share/fish/vendor_completions.d"
         set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
     end
-
+    fzf_configure_bindings --directory=\ct --git_log=\cg --processes=\cq
     fish_add_path /opt/homebrew/opt/openjdk/bin
     . /opt/homebrew/opt/asdf/libexec/asdf.fish
     export LDFLAGS="-L/opt/homebrew/opt/capstone/lib"
