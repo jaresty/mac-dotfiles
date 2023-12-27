11cd6a35b0330cb2935d061c62558f6e541a75a3
Joshua Aresty <jaresty@gmail.com>
2023-12-26 22:10:51 -0800

    Fixed up spelling of "openapi" in set-openapi-key.py

--- talon-settings/set-openapi-key.py
+++ talon-settings/set-openapi-key.py
@@ -2,4 +2,4 @@
 
 import os
 with open(os.path.expanduser('~/.openapi-key'), 'r') as file:
-    os.environ['OPENAPI_KEY'] = file.read().strip()
+    os.environ['OPENAI_API_KEY'] = file.read().strip()
