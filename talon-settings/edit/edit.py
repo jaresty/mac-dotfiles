4fd8aff82eeeabbc5fc3fc7f19df35bcc16c4508
Joshua Aresty <jaresty@gmail.com>
2025-03-09 16:04:47 -0700

    on pusher should push the rest of the line to the next line

--- talon-settings/edit/edit.py
+++ talon-settings/edit/edit.py
@@ -8,12 +8,10 @@ mod = Module()
 class Actions:
     def push_keys(keys: list[str] = ["space"]):
         """Insert these keys one to the right of the cursor position"""
-        actions.edit.right()
         for key in keys:
             actions.key(key)
         for key in keys:
             actions.edit.left()
-        actions.edit.left()
 
     def repush_keys(keys: list[str] = ["space"]):
         """Insert these keys one to the left of the cursor position"""
@@ -25,7 +23,7 @@ class Actions:
 
     def pusher_keys(keys: str = ""):
         """Insert these keys on the next line after the cursor position"""
-        actions.edit.line_insert_down()
+        actions.key("enter")
         actions.key(keys)
         actions.edit.up()
         actions.edit.line_end()
