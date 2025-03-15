ae21b652ff4743f1d4eca2feb5904ae17475a9fc
Joshua Aresty <jaresty@gmail.com>
2025-03-15 14:39:12 -0700

    Refactor spoken search rules to use more targeted captures

--- talon-settings/edit/edit.py
+++ talon-settings/edit/edit.py
@@ -5,14 +5,33 @@ mod = Module()
 
 
 @mod.capture(
-    rule="{user.code_formatter} <user.prose> | {user.code_formatter} paste | echo"
+    rule="<user.formatted_prose> | <user.formatted_paste> | <user.last_phrase>"
 )
 def spoken_search(m) -> str:
-    if hasattr(m, "prose"):
-        return actions.user.formatted_text(m.prose, m.code_formatter)
-    elif hasattr(m, "code_formatter"):
-        return actions.user.reformat_text(actions.clip.text(), m.code_formatter)
+    return m.formatted_prose or m.formatted_paste or m.last_phrase
 
+
+# @mod.capture(rule="curse [<user.formatters>] <user.cursorless_target>")
+# def cursorless_formatted(m) -> str:
+#     target_text = actions.user.cursorless_get_text(m.cursorless_target)
+#     if hasattr(m, "formatters"):
+#         return actions.user.formatted_text(target_text, m.code_formatter)
+
+#     return target_text
+
+
+@mod.capture(rule="<user.formatters> <user.prose>")
+def formatted_prose(m) -> str:
+    return actions.user.formatted_text(m.prose, m.formatters)
+
+
+@mod.capture(rule="<user.formatters> paste")
+def formatted_paste(m) -> str:
+    return actions.user.reformat_text(actions.clip.text(), m.formatters)
+
+
+@mod.capture(rule="echo")
+def last_phrase(_) -> str:
     return actions.user.get_last_phrase()
 
 
