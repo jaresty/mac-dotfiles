01faccc273fe19c345f86d3e9a879dc38d9bb1ce
Joshua Aresty <jaresty@gmail.com>
2025-03-30 18:47:48 -0700

    Add a helper for working with paints

--- talon-settings/edit/edit.py
+++ talon-settings/edit/edit.py
@@ -1,3 +1,4 @@
+import re
 from talon import Context, Module, actions
 
 ctx = Context()
@@ -58,6 +59,17 @@ def locate_character(line_text: str, character: str, offset: int):
     return line_text.lower().find(character) + offset
 
 
+def locate_paint(line_text: str, offset: int):
+    pattern = r"(\S\s+\S)"
+    matches = re.search(pattern, line_text)
+    if matches is None:
+        return len(line_text)
+    elif offset == 1:
+        return matches.end() - 1
+    else:
+        return matches.start() + 1
+
+
 @mod.action_class
 class Actions:
     def poke_keys(keys: list[str] = ["space"]):
@@ -102,6 +114,28 @@ class Actions:
         for _ in range(index_of_character):
             actions.edit.left()
 
+    def go_next_paint(offset: int):
+        """Go to the next whitespace to limited word"""
+        actions.edit.extend_line_end()
+        actions.sleep("300ms")
+        line_end_text = actions.edit.selected_text()
+        actions.edit.left()
+        index_of_character = locate_paint(line_end_text, offset)
+
+        for _ in range(index_of_character):
+            actions.edit.right()
+
+    def go_previous_paint(offset: int):
+        """Go to the previous whitespace to limited word"""
+        actions.edit.extend_line_start()
+        actions.sleep("300ms")
+        line_start_text = actions.edit.selected_text()[::-1]
+        actions.edit.right()
+        index_of_character = locate_paint(line_start_text, offset)
+
+        for _ in range(index_of_character):
+            actions.edit.left()
+
     def select_to_next_character(character: str, offset: int):
         """Select to the next character specified"""
         initially_selected_text = actions.edit.selected_text()
@@ -138,6 +172,42 @@ class Actions:
         for _ in range(initial_position + index_of_character):
             actions.edit.extend_left()
 
+    def select_to_next_paint(offset: int):
+        """Select to the next paint"""
+        initially_selected_text = actions.edit.selected_text()
+        initial_position = len(initially_selected_text) + 1
+
+        actions.edit.extend_line_end()
+        actions.sleep("200ms")
+        line_end_text = actions.edit.selected_text()
+
+        if initial_position > len(line_end_text):
+            initial_position = 0
+        additional_text = line_end_text[initial_position:]
+
+        actions.edit.left()
+        index_of_character = locate_paint(additional_text, offset)
+
+        for _ in range(initial_position + index_of_character):
+            actions.edit.extend_right()
+
+    def select_to_previous_paint(offset: int):
+        """Select to the previous paint"""
+        initially_selected_text = actions.edit.selected_text()
+        initial_position = len(initially_selected_text) + 1
+        actions.edit.extend_line_start()
+        actions.sleep("200ms")
+        line_start_text = actions.edit.selected_text()[::-1]
+
+        if initial_position > len(line_start_text):
+            initial_position = 0
+        additional_text = line_start_text[initial_position:]
+        actions.edit.right()
+        index_of_character = locate_paint(additional_text, offset)
+
+        for _ in range(initial_position + index_of_character):
+            actions.edit.extend_left()
+
     def delete_to_next_character(character: str, offset: int):
         """Move the cursor to the next character specified"""
         actions.edit.extend_line_end()
@@ -154,6 +224,26 @@ class Actions:
         line_start_text = actions.edit.selected_text()[::-1]
         actions.edit.right()
         index_of_character = locate_character(line_start_text, character, offset)
+
+        for _ in range(index_of_character):
+            actions.key("backspace")
+
+    def delete_to_next_paint(offset: int):
+        """Move the cursor to the next character specified"""
+        actions.edit.extend_line_end()
+        line_end_text = actions.edit.selected_text()
+        actions.edit.left()
+        index_of_character = locate_paint(line_end_text, offset)
+
+        for _ in range(index_of_character):
+            actions.key("delete")
+
+    def delete_to_previous_paint(offset: int):
+        """Move the cursor to the previous character specified"""
+        actions.edit.extend_line_start()
+        line_start_text = actions.edit.selected_text()[::-1]
+        actions.edit.right()
+        index_of_character = locate_paint(line_start_text, offset)
 
         for _ in range(index_of_character):
             actions.key("backspace")
