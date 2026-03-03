eabd447c562e6b0bd950c8c5f531474fc4b9c30d
Joshua Aresty <jaresty@gmail.com>
2026-03-03 14:38:59 -0800

    fix: correct jetbrains forward/back directions

--- talon-settings/forward_back/forward_back_jetbrains.py
+++ talon-settings/forward_back/forward_back_jetbrains.py
@@ -12,7 +12,7 @@ app: jetbrains
 @ctx.action_class("user")
 class UserActions:
     def go_forward():
-        actions.user.idea("action Back")
+        actions.user.idea("action Forward")
 
     def go_back():
-        actions.user.idea("action Forward")
+        actions.user.idea("action Back")
